a50820533dfd8777b0bf68b0df482330fec4fa1a
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-06-28 16:23:33 -0500

    Login listo by Hector

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,20 @@
     </style>
 </head>
 <body>
-    <div>
+    <div class="min-h-screen flex items-center justify-center">
+    <div class="centered-container">
+    
+    
+            
     <form method="POST" action="{{ route('login') }}" class="form-container">
         @csrf
-
+        <div class="flex items-center justify-center">
+        <h2 class="mx-auto mb-4 text-green-400 font-bold">Login</h2>
+        </div>
+        <img src="{{ asset('images/mochi.gif') }}" alt="Login" class="mx-auto mb-4 w-32  object-cover">
         <div class="form-group">
             <label for="username">Nombre de usuario</label>
-            <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
+            <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus placeholder="Ingrese su nombre de Usuario">
             @error('username')
                 <span>{{ $message }}</span>
             @enderror
@@ -29,7 +36,7 @@
 
         <div class="form-group">
             <label for="password">Contraseña</label>
-            <input id="password" type="password" name="password" required>
+            <input id="password" type="password" name="password" placeholder="Ingrese su Contraseña" required>
             @error('password')
                 <span>{{ $message }}</span>
             @enderror
@@ -43,9 +50,10 @@
         </div>
         <div class="flex items-center justify-center">
         <button type="submit" class="mr-4 submit-button">Iniciar sesión</button>
-        <a href="{{ route('register') }}" class=" submit-button hover:bg-purple-500">Registrarse</a>
+        <a href="{{ route('register') }}" class=" submit-button hover:bg-green-400">Registrarse</a>
         </div>
     </form>
+    </div>
 </div>
 </body>
 </html>
