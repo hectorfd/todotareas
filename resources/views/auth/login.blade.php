9631dcc53324b51a742df841243dd755b26ba44f
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-06-28 13:42:41 -0500

    configuracion de estilos falta composicion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,55 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
 
     <title>Login</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
-        .form-container {
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #ffffff;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-        }
-        .form-group {
-            margin-bottom: 15px;
-        }
-        .form-group label {
-            display: block;
-            color: #4a4a4a;
-            margin-bottom: 5px;
-            font-weight: bold;
-        }
-        .form-group input[type="text"],
-        .form-group input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #cccccc;
-            border-radius: 4px;
-        }
-        .form-group input[type="checkbox"] {
-            margin-right: 10px;
-        }
-        .form-group span {
-            color: #ff0000;
-            font-size: 14px;
-        }
-        .submit-button {
-            background-color: #007bff;
-            color: #ffffff;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        .submit-button:hover {
-            background-color: #0056b3;
-        }
+        
+        
     </style>
 </head>
 <body>
+    <div>
     <form method="POST" action="{{ route('login') }}" class="form-container">
         @csrf
 
@@ -78,9 +41,12 @@
                 Recordarme
             </label>
         </div>
-
-        <button type="submit" class="submit-button">Iniciar sesión</button>
+        <div class="flex items-center justify-center">
+        <button type="submit" class="mr-4 submit-button">Iniciar sesión</button>
+        <a href="{{ route('register') }}" class=" submit-button hover:bg-purple-500">Registrarse</a>
+        </div>
     </form>
+</div>
 </body>
 </html>
 
