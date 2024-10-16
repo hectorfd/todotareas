d9646c550be031104977240354d9ad38dbb716e0
ale <156329901+Jimena77777777777777@users.noreply.github.com>
2024-10-15 20:25:18 -0500

    quiero un negro uwu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'TodoList') }}</title>
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
+
+
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
